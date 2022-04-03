28eeec62b4c2e9aed4f0f7e6c3c35af3f54c0b0a
Cyrus Majd <cyrusmajd2001@gmail.com>
2022-04-03 09:39:07 -0400

    merged codes in readchars.py

--- input.py
+++ input.py
@@ -2,7 +2,11 @@ import subprocess
 proc = subprocess.Popen(['candump','can0,238:FF'],stdout=subprocess.PIPE)
 #works in python 3.0+
 #for line in proc.stdout:
+
+# VALUE OF N: 160 is the middle. anything less is LEFT. anything greater is RIGHT.
 for line in iter(proc.stdout.readline,''):
     s = line.rstrip().decode("UTF-8")
     n = int(s[28:30],16)
     print(n)
+
+
--- readChars.py
+++ readChars.py
@@ -14,15 +14,24 @@ def threadFunc():
             break
         pyautogui.press('a', presses=50)
 
-time.sleep(2)
-
 # Create a Thread with a function without any arguments
 th = threading.Thread(target=threadFunc)
 th.start()
 
-# Print some messages on console
-while(True):
+## reading data from the car.
+import subprocess
+proc = subprocess.Popen(['candump','can0,238:FF'],stdout=subprocess.PIPE)
 
+# Print some messages on console
+for line in iter(proc.stdout.readline,''):
+    s = line.rstrip().decode("UTF-8")
+    n = int(s[28:30],16)
+    print(n)
+
+    if (n < 140): #turn left
+        pyautogui.press('left', presses=10)
+    elif (n > 180): # turn right
+        pyautogui.press('right', presses=10)
 
     if keyboard.read_key() == "q":
         print("You pressed q. quitting.")
