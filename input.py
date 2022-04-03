91d322f27c7d65e78469753af0ec54e3f1bfb695
CyrusTheVirus <cyrusmajd2001@gmail.com>
2022-04-03 07:43:42 -0400

    update

--- input.py
+++ input.py
@@ -1,5 +1,5 @@
 import subprocess
-proc = subprocess.Popen(['candump','can0'],stdout=subprocess.PIPE)
+proc = subprocess.Popen(['candump','can0,4E0:FF'],stdout=subprocess.PIPE)
 #works in python 3.0+
 #for line in proc.stdout:
 for line in iter(proc.stdout.readline,''):
